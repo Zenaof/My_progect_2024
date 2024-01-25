521bafd9ac22acbd3f3aadcc9e80e625b6735773
Zenaof <113383146+Zenaof@users.noreply.github.com>
2024-01-25 18:43:29 +0300

    .

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -6,17 +6,13 @@ using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
-    private Button SinglePlayBut;
-    private Button MultiPlayBut;
-    private Button SettingsBut;
-    private Button ExitBut;
-
+    [SerializeField]
+    private GameObject mainMenu;
+    [SerializeField]
+    private GameObject settingsMenu;
+    
     private void Awake()
     {
-        SinglePlayBut = GetComponent<Button>();
-        MultiPlayBut = GetComponent<Button>();
-        SettingsBut = GetComponent<Button>();
-        ExitBut = GetComponent<Button>();
     }
     
    public void goSinglePlay()
